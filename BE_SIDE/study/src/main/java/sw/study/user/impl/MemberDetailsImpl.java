158b58a27febcc2263bb33734b72e9462bfd2c70
junhyunlim73 <limjh0703@gmail.com>
2024-10-24 22:15:06 +0900

    ♻️Refactor: 권한을 맴버 role로 적용

--- BE_SIDE/study/src/main/java/sw/study/user/impl/MemberDetailsImpl.java
+++ BE_SIDE/study/src/main/java/sw/study/user/impl/MemberDetailsImpl.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import sw.study.user.domain.Member;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -22,7 +21,7 @@ public class MemberDetailsImpl implements UserDetails {
 
     @Override // 권한 반환
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        return List.of(new SimpleGrantedAuthority(member.getRole().name()));
     }
 
     @Override
