90685479ac1b90c18f9ca674080d2f674cf07d38
junhyunlim73 <limjh0703@gmail.com>
2024-10-24 18:18:17 +0900

    ♻️Refactor: 맴버 DTO 변경
    - password 삭제
    - 알림 리스트 추가

--- BE_SIDE/study/src/main/java/sw/study/user/dto/MemberDto.java
+++ BE_SIDE/study/src/main/java/sw/study/user/dto/MemberDto.java
@@ -2,11 +2,14 @@ package sw.study.user.dto;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class MemberDto {
     private String email;
-    private String password;
     private String nickname;
     private String profile;
     private String introduce;
+    List<NotificationSettingDTO> settings = new ArrayList<>();
 }
