682efe0dcded94ae8ce6af71e9c9df6332d2e7c5
Oyisbe <onyu8407@naver.com>
2024-11-09 01:11:52 +0900

    :sparkles: Feat: StudyGroup API- Swagger Annotations added
    - 추후 인터페이스로 별도로 작성 예정
    (ApiDocumentation)
    - PathVariable 로 구성한 API 는 tryOut에서 테스트가 안됨. 수정 필요

--- BE_SIDE/study/src/main/java/sw/study/studyGroup/dto/CreateStudyGroup.java
+++ BE_SIDE/study/src/main/java/sw/study/studyGroup/dto/CreateStudyGroup.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Data
 public class CreateStudyGroup {
     private String groupName;
-    private String Description;
+    private String description;
     private List<String> selectedNicknames;
     private String leaderNickname;
 }
