9f8cb4d879c5847e74a0005bb595560896d3a782
xo482 <ksh990409@naver.com>
2024-11-27 00:17:53 +0900

    ✨Feat: 대댓글 삭제, 좋아요, 좋아요취소, 신고 기능 구현
    - 대댓글 관련 기능 추가 및 테스트코드 추가

--- BE_SIDE/study/src/main/java/sw/study/community/service/CommentService.java
+++ BE_SIDE/study/src/main/java/sw/study/community/service/CommentService.java
@@ -139,6 +139,35 @@ public class CommentService {
     }
 
     /**
+     * 대댓글 좋아요 취소
+     */
+    public void cancelReplyLike(Long postId, Long commentId, Long replyId, Long memberId) {
+        // 게시글, 댓글, 대댓글, 좋아요를 취소하려는 사람 조회
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("해당 게시글을 찾을 수 없습니다."));
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new CommentNotFoundException("해당 댓글을 찾을 수 없습니다."));
+        Comment reply = commentRepository.findById(replyId)
+                .orElseThrow(() -> new CommentNotFoundException("해당 대댓글을 찾을 수 없습니다."));
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new UserNotFoundException("해당 사용자를 찾을 수 없습니다."));
+        CommentLike commentLike = commentLikeRepository.findByCommentAndMember(reply, member)
+                .orElseThrow(() -> new LikeNotFoundException("좋아요가 존재하지 않습니다"));
+
+        // 댓글이 해당 게시글에 속하는지 검증
+        if (!post.getComments().contains(comment) || !comment.getChild().contains(reply)) {
+            throw new CommentNotBelongToPostException("대댓글이 해당 게시글에 속하지 않거나, 댓글에 속하지 않습니다.");
+        }
+
+        // 대댓글 좋아요 취소
+        reply.deletedCommentLike(commentLike);
+        commentLikeRepository.delete(commentLike);
+        log.info("대댓글에 성공적으로 좋아요 취소: replyId={}, commentId={}, postId={}, memberId={}", replyId, commentId, postId, memberId);
+    }
+
+
+    /**
      * 댓글 신고
      */
     public Long report(ReportRequest reportRequest, Long postId, Long commentId) {
@@ -167,6 +196,84 @@ public class CommentService {
         comment.incrementReportCount();
         Long reportId = reportService.save(report);
         log.info("댓글 신고 요청 완료: commentId = {}, reporterId = {}, targetMemberId = {}", commentId, reportId, targetMember.getId());
+        return reportId;
+    }
+
+    /**
+     * 대댓글 삭제
+     */
+    public void deleteReply(Long postId, Long commentId, Long replyId) {
+        // 게시글, 댓글, 대댓글 조회
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("해당 게시글을 찾을 수 없습니다."));
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new CommentNotFoundException("해당 댓글을 찾을 수 없습니다."));
+        Comment reply = commentRepository.findById(replyId)
+                .orElseThrow(() -> new CommentNotFoundException("해당 대댓글을 찾을 수 없습니다."));
+
+        // 댓글이 해당 게시글에 속하는지 검증
+        if (!post.getComments().contains(comment) || !comment.getChild().contains(reply)) {
+            throw new CommentNotBelongToPostException("대댓글이 해당 게시글에 속하지 않거나, 댓글에 속하지 않습니다.");
+        }
+
+        // 대댓글 삭제(논리적 삭제)
+        reply.deleteComment();
+        log.info("대댓글이 성공적으로 삭제(논리적): replyId={}, commentId={}, postId={}", replyId, commentId, postId);
+    }
+
+    /**
+     * 대댓글 좋아요
+     */
+    public void addReplyLike(Long postId, Long commentId, Long replyId, Long memberId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("해당 게시글을 찾을 수 없습니다."));
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new CommentNotFoundException("해당 댓글을 찾을 수 없습니다."));
+        Comment reply = commentRepository.findById(replyId)
+                .orElseThrow(() -> new CommentNotFoundException("해당 대댓글을 찾을 수 없습니다."));
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new UserNotFoundException("해당 사용자를 찾을 수 없습니다."));
+
+        if (!post.getComments().contains(comment) || !comment.getChild().contains(reply)) {
+            throw new CommentNotBelongToPostException("대댓글이 해당 게시글에 속하지 않거나, 댓글에 속하지 않습니다.");
+        }
+
+        // 대댓글 좋아요
+        CommentLike commentLike = CommentLike.createCommentLike(reply, member);
+        commentLikeRepository.save(commentLike);
+        log.info("대댓글에 성공적으로 좋아요: replyId={}, commentId={}, postId={}, memberId={}", replyId, commentId, postId, memberId);
+    }
+
+    /**
+     * 대댓글 신고
+     */
+    public Long reportReply(ReportRequest reportRequest, Long postId, Long commentId, Long replyId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("해당하는 게시글을 찾을 수 없습니다."));
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new PostNotFoundException("해당하는 댓글을 찾을 수 없습니다."));
+        Comment reply = commentRepository.findById(replyId)
+                .orElseThrow(() -> new CommentNotFoundException("해당 대댓글을 찾을 수 없습니다."));
+
+        Member targetMember = memberRepository.findById(reply.getMember().getId())
+                .orElseThrow(() -> new UserNotFoundException("해당하는 피신고자를 찾을 수 없습니다."));
+        Member reporter = memberRepository.findById(reportRequest.getReporterId())
+                .orElseThrow(() -> new UserNotFoundException("해당하는 신고자를 찾을 수 없습니다."));
+
+        if (!post.getComments().contains(comment) || !comment.getChild().contains(reply)) {
+            throw new CommentNotBelongToPostException("대댓글이 해당 게시글에 속하지 않거나, 댓글에 속하지 않습니다.");
+        }
+
+        Report report = Report.createReport(reporter, targetMember, replyId,
+                reportRequest.getDescription(),
+                reportRequest.getReportTargetType(),
+                reportRequest.getReportReason(),
+                ReportStatus.PENDING);
+
+        // 대댓글 신고 수 증가
+        reply.incrementReportCount();
+        Long reportId = reportService.save(report);
+        log.info("대댓글 신고 요청 완료: replyId = {}, reporterId = {}, targetMemberId = {}", replyId, reportId, targetMember.getId());
         return reportId;
     }
 }
