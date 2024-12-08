17200f3d313167c7d5fa885ef1c8147edf91ef63
Oyisbe <onyu8407@naver.com>
2024-12-09 02:16:01 +0900

    :recycle: Refactor: 전역 예외 처리 클래스 Logger - 로깅 추가
    - 500 리턴하는 모든 기타 예외 처리 관련.
    - Custom Ex 제외한 케이스 관련 : 디버깅 용이하도록 함

--- BE_SIDE/study/src/main/java/sw/study/exception/GlobalExceptionHandler.java
+++ BE_SIDE/study/src/main/java/sw/study/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package sw.study.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     // BaseException 처리 (구체적 예외)
     @ExceptionHandler(BaseException.class)
@@ -19,6 +23,7 @@ public class GlobalExceptionHandler {
     // 모든 기타 예외 처리 (500 에러)
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleUnexpectedException(Exception ex) {
+        log.error("BaseException occurred: {}", ex.getMessage(), ex); // 로그 기록
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("서버 오류가 발생했습니다.");
     }
