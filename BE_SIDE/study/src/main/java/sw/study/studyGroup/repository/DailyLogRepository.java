6bdc8857e1dfd45bbf6f159fbe2235a6727766a8
Oyisbe <onyu8407@naver.com>
2024-12-14 23:43:58 +0900

    :recycle: Refactor: DailyLog PK 관련 타입 혼용 수정
    Controller, Service, Repository 간
    Pk 핸들링 시 ( Long / long ) 혼용한 코드를 Long 으로 일관적으로 다 수정함.
    
    - AutoBoxing / unBoxing 으로 실행 자체는 되었던 상황.

--- BE_SIDE/study/src/main/java/sw/study/studyGroup/repository/DailyLogRepository.java
+++ BE_SIDE/study/src/main/java/sw/study/studyGroup/repository/DailyLogRepository.java
@@ -1,6 +1,5 @@
 package sw.study.studyGroup.repository;
 
-import org.springframework.cglib.core.Local;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,5 +10,5 @@ import java.time.LocalDateTime;
 @Repository
 public interface DailyLogRepository extends JpaRepository<DailyLog, Long>{
 
-    Page<DailyLog> findAllByStudyGroup_IdAndCreatedAtBetween(long groupId, LocalDateTime start, LocalDateTime end, Pageable pageable);
+    Page<DailyLog> findAllByStudyGroup_IdAndCreatedAtBetween(Long groupId, LocalDateTime start, LocalDateTime end, Pageable pageable);
 }
