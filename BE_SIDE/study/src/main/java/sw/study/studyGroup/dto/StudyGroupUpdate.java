d24d9bb387988d817f1a2c25cfece7b8f30eb93d
Oyisbe <onyu8407@naver.com>
2025-01-04 00:06:28 +0900

    :recycle: Refactor: 그룹 정보 수정 DTO 관련 수정
    기본 생성자 추가 이후 동작 확인함
    ->존재하지 않으면 역직렬화 불가능
    (넘겨받은 JSON 데이터를 자바 객체로 변환할때, 기본 생성자로 객체 생성 후 필드에 매핑하기 때문)

--- BE_SIDE/study/src/main/java/sw/study/studyGroup/dto/StudyGroupUpdate.java
+++ BE_SIDE/study/src/main/java/sw/study/studyGroup/dto/StudyGroupUpdate.java
@@ -2,12 +2,14 @@ package sw.study.studyGroup.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 // 스터디그룹 정보 수정 Dto
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class StudyGroupUpdate {
     private String groupName;
     private String description;
